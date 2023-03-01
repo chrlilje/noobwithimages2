7f6c2d8a8cff31edb0cb9031c5748f8e8820b381
chrlilje <christian@liljedahl.dk>
2023-03-01 19:12:51 +0100

    sss

--- mySketch.js
+++ mySketch.js
@@ -3,13 +3,18 @@ function preload(){
 	myImg = loadImage("baum.png");	
 }
 
+let posx = 0;
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	createCanvas(600, 400);
 	myImg.resize(200,0);
 	imageMode(CENTER);
 }
 
 function draw() {
 	background(100);
-	image(myImg, width/2, height/2);
+	image(myImg, posx, height/2);
+	posx += 5;
+	if(posx > width){
+		posx = 0;
+	}
 }
